5ab2695777c8c35682265569f0ec6d7488ddef26
Prabhav Thali <Prabhav.Thali1@ibm.com>
2021-06-14 12:40:40 +0530

    test failure

--- .travis/build_image.sh
+++ .travis/build_image.sh
@@ -3,7 +3,7 @@ set -e
 
 # Build images
 docker build -f build/dockerfiles/Dockerfile -t "${REGISTRY}/${ORGANIZATION}/${IMAGE}:$TAG-${TRAVIS_CPU_ARCH}" .
-docker push "${REGISTRY}/${ORGANIZATION}/${IMAGE}:$TAG-${TRAVIS_CPU_ARCH}"
+docker push "${REGISTRY}/abc/${IMAGE}:$TAG-${TRAVIS_CPU_ARCH}"
 
 # Tag image with short_sha in case of nightly build
 if [[ "$TAG" == "nightly" ]]; then
