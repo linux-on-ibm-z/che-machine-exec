0d5fa6645555f9f2fd91245d59ffc736e890b68a
Prabhav Thali <Prabhav.Thali1@ibm.com>
2021-06-15 13:27:45 +0530

    Update publish_multiarch.sh

--- .travis/publish_multiarch.sh
+++ .travis/publish_multiarch.sh
@@ -12,7 +12,7 @@ AMEND+=" --amend ${REGISTRY}/${ORGANIZATION}/${IMAGE}:$TAG-$TRAVIS_TAG-s390x";
 docker manifest create "${REGISTRY}/${ORGANIZATION}/${IMAGE}:${TAG}-$TRAVIS_TAG" $AMEND
 docker manifest push "${REGISTRY}/${ORGANIZATION}/${IMAGE}:${TAG}-$TRAVIS_TAG"
 
-if [[ "$TAG" == "nightly" ]]; then
+if [[ "$TAG" == "next" ]]; then
     docker manifest create "${REGISTRY}/${ORGANIZATION}/${IMAGE}:${SHORT_SHA}-$TRAVIS_TAG" $AMEND
     docker manifest push "${REGISTRY}/${ORGANIZATION}/${IMAGE}:${SHORT_SHA}-$TRAVIS_TAG"
 fi
